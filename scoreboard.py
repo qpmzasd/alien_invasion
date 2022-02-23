571587c3159bfd308735e7a54d1f0124daad6f0c
qpmzasd <qpmzasd@163.com>
2022-02-23 15:57:26 +0800

    0223-2

--- scoreboard.py
+++ scoreboard.py
@@ -13,12 +13,14 @@ class Scoreboard:
         # 显示得分信息时使用的字体设置
         self.text_color = (30, 30, 30)
         self.font = pygame.font.SysFont(None, 48)
-        # 准备初始得分图像
+        # 准备初始得分图像和当前最高得分
         self.prep_score()
+        self.prep_high_score()
 
     def prep_score(self):
         """将得分转换为一幅渲染的图像"""
-        score_str = str(self.stats.score)
+        rounded_score = round(self.stats.score, -1)
+        score_str = "{:,}".format(rounded_score)
         self.score_image = self.font.render(score_str, True, self.text_color, self.settings.bg_color)
 
         # 在屏幕右上角显示扥分
@@ -26,6 +28,24 @@ class Scoreboard:
         self.score_rect.right = self.screen_rect.right - 20
         self.score_rect.top = 20
 
+    def prep_high_score(self):
+        """将最高得分转换为一幅渲染的图像"""
+        high_score = round(self.stats.high_score, -1)
+        high_score_str = "{:,}".format(high_score)
+        self.high_score_image = self.font.render(high_score_str, True, self.text_color, self.settings.bg_color)
+
+        # 在屏幕右上角显示扥分
+        self.high_score_rect = self.high_score_image.get_rect()
+        self.high_score_rect.centerx = self.screen_rect.centerx
+        self.high_score_rect.top = self.score_rect.top
+
     def show_score(self):
         """在屏幕上显示得分"""
         self.screen.blit(self.score_image, self.score_rect)
+        self.screen.blit(self.high_score_image,self.high_score_rect)
+
+    def check_high_score(self):
+        """检查是否诞生了新的最高分"""
+        if self.stats.score > self.stats.high_score:
+            self.stats.high_score = self.stats.score
+            self.prep_high_score()
